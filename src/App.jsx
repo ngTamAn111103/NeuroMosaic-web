0bdce174f2264cfbbbfad2b2fd3a63364fbb75b4
ngTamAn111103 <ng.t.ann2003@gmail.com>
2025-12-07 23:06:48 +0700

    Cấu hình được canvas 3D, hiển thị được ảnh và môi trường 3D

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,50 @@
 import { useState } from "react";
+// Canvas
+import { Canvas, useLoader } from "@react-three/fiber";
+
+// Data ảnh
+import data_images from "./data_images.json";
+import { TextureLoader } from "three";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const ImageItem = ({ url, position }) => {
+    // Tải ảnh và biến thành Texture -> dán lên mesh
+    const texture = useLoader(TextureLoader, url);
+    return (
+      <mesh position={position}>
+        {/* Hình dạng */}
+        <planeGeometry args={[2, 2]} />
+        {/* toneMapped={false}: không bị “wash out”. */}
+        <meshBasicMaterial map={texture} toneMapped={false} />
+      </mesh>
+    );
+  };
 
   return (
-    <>
-      <div className="ml-4 flex h-24 border-2 border-gray-300 p-3 text-gray-700 shadow-md">
-        <h1 class="text-3xl font-bold text-red-500 underline">Hello world!</h1>
-      </div>
-    </>
+    <div className="relative h-screen w-full bg-black">
+      {/* UI Overlay */}
+      <h1 className="absolute top-5 left-5 z-10 bg-white">
+        {/* Tên dự án */}
+        <div className="">
+          <h1 className="bg-linear-to-r from-cyan-400 to-purple-500 text-3xl font-bold">
+            NEUROMOSAIC
+          </h1>
+        </div>
+      </h1>
+
+      {/* Thế giới 3D */}
+      <Canvas camera={{ position: [0, 0, 5], fov: 60 }}>
+        {/* Ánh sáng (0/1: Tối/Sáng) */}
+        <ambientLight intensity={1} />
+        {/* // mesh = geometry + material */}
+        {data_images.map((img) => (
+          <ImageItem key={img.id} url={img.thumbnail} position={img.position} />
+        ))}
+        {/* Camera điều khiển chuột */}
+        <OrbitControls />
+      </Canvas>
+    </div>
   );
 }
 
